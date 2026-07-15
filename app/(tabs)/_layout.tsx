a045405a45660571644962dc76543e302a82a5f2
nenakassandraencabo <nenakassandraencabo@gmail.com>
2026-07-15 14:02:46 +0800

    Added profile

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -18,14 +18,21 @@ export default function TabLayout() {
     },
   }}
 >
-
-    >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
           tabBarIcon: ({ color, focused }) => (
             <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'person-sharp' : 'person-outline'} color={color} size={24} />
           ),
         }}
       />
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function ProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Profile screen</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f51de3',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
